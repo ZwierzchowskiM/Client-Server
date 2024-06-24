dadd89ead5c6d02a4a50a631bb7e409fa9c94b79
ZwierzchowskiM <marcin.zwierzchowski1@gmail.com>
2024-06-25 00:11:17 +0200

    Move database credentials from DatabaseManager class to db.properties file

--- src/main/java/org/zwierzchowski/marcin/utils/DataBaseManager.java
+++ src/main/java/org/zwierzchowski/marcin/utils/DataBaseManager.java
@@ -1,26 +1,46 @@
 package org.zwierzchowski.marcin.utils;
 
-import org.jooq.tools.JooqLogger;
-import org.zwierzchowski.marcin.exception.DatabaseConnectionException;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
+import lombok.extern.log4j.Log4j2;
+import org.jooq.tools.JooqLogger;
+import org.zwierzchowski.marcin.exception.DatabaseConnectionException;
+
+@Log4j2
 public class DataBaseManager {
 
-  String userName = "marcin";
-  String password = "passs";
-  String url = "jdbc:postgresql://localhost:5432/client_server";
+  String username;
+  String password;
+  String url;
+
+  public DataBaseManager() {
+    getCredentials();
+  }
 
   public Connection getConnection() throws DatabaseConnectionException {
     JooqLogger.globalThreshold(org.jooq.tools.JooqLogger.Level.WARN);
     try {
-      Connection conn = DriverManager.getConnection(url, userName, password);
-      return conn;
+        return DriverManager.getConnection(url, username, password);
     } catch (SQLException e) {
-      e.printStackTrace();
+      log.error("Error connecting to database", e);
       throw new DatabaseConnectionException("Cannot connect to database");
+    }
+  }
+
+  private void getCredentials() {
+
+    Properties properties = new Properties();
+
+    try {
+      properties.load(DataBaseManager.class.getClassLoader().getResourceAsStream("db.properties"));
+      username = properties.getProperty("username");
+      password = properties.getProperty("password");
+      url = properties.getProperty("url");
+    } catch (Exception e) {
+      e.printStackTrace();
     }
   }
 }
